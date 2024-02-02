9d1500175d215d69a41899ffcb8a935c630b31cf
Shibaz98 <shibaz98@gmail.com>
2024-02-02 15:08:08 +0000

    Second commit with working switch case

--- message.js
+++ message.js
@@ -8,14 +8,35 @@ const leisure = {
 
 
 let randomIndex = (num) =>{
-    return Math.floor(Math.random() * num)
+    return Math.floor(Math.random() * num)   // This function allows me to generatre a random number, num argument is the length of the given array so I don't need to stress about it being a fixed number
 }
 
+let randomMessageArray = []; 
 
 for (let key in leisure){
-    let optionIndex = randomIndex(leisure[key].length)
+    let optionIndex = randomIndex(leisure[key].length) // .length gives me the length of the entire array and then randomIndex takes that number as an argument
     
-
+        switch(key){
+            case 'gaming':
+                randomMessageArray.push(`Since it's your day off, you should play a console game, I recommend: ${leisure[key][optionIndex]}`)
+                break;
+            case 'physical activity':
+                randomMessageArray.push(`Don't spend the entire day playing games, definitely do a physical activity such as: ${leisure[key][optionIndex]}`) 
+                break;
+            case 'food options':
+                randomMessageArray.push(`For dinner, you should have: ${leisure[key][optionIndex]}`)
+                break;      
+        }
+    
+    /*    
+    if(key === 'gaming'){
+        console.log(`testing out the if statement ${leisure[key][optionIndex]}`)
+    } else if(key === 'physical activity'){
+        console.log(`testing out the if statement ${leisure[key][optionIndex]}`)  <<< The above also works as an If statement, just put this here for fun
+    } else if(key === 'food options'){
+        console.log(`testing out the if statement ${leisure[key][optionIndex]}`)
+    }  
+    */ 
     
 
 }
